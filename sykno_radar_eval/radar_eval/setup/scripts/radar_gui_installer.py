8c01c578baf6effad8103e2f1bd39c7d2eed0563
Jan Paatsch <janphilipp.paatsch@fau.de>
2024-05-31 17:14:15 +0200

    distribution rdy folder (sykno_radar_eval)

---
+++ sykno_radar_eval/radar_eval/setup/scripts/radar_gui_installer.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+# Add the dependencies folder to the system path
+script_dir = os.path.dirname(__file__)
+
+# Go back four directories
+for _ in range(3):
+    script_dir = os.path.dirname(script_dir)
+
+# Construct the path to the dependencies folder
+dependencies_path = os.path.join(script_dir)
+
+# Add the dependencies folder to the system path
+sys.path.append(dependencies_path)
+
+import __init__
+
+# Import the required modules from the dependencies folder
+try:
+    import radar_eval.gui.radar_eval_qt_gui as mira_eval_gui
+except ImportError as e:
+    print(f"ImportError: {e}")
+    sys.exit(1)
+
+# Execute the function
+try:
+    mira_eval_gui.MIRA_MAIN_GUI.mira_gui_main()
+except Exception as e:
+    print(f"Error: {e}")
+    sys.exit(1)
